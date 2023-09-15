6d3c84d175840be65f61f9edc082b2512a07491c
AceMido <acemido2004@gmail.com>
2023-09-15 17:12:26 +0300

    made it handle * to make it easier and made it run forever until a dir is found

--- which.sh
+++ which.sh
@@ -1,60 +1,114 @@
 #!/bin/bash
-
-echo "use 'source script.sh' or '. script.sh' to run this on ur current bash enviroment"
-
-echo -e "Where do u want to go to? : \c"
+# Use source command or simply . script to run this
+while true; do
+echo -e "Where do you want to go to? : \c"
 read dir
-if [ "$dir" == "DataStructures" ]; then
 
-	echo -e "Which lang? \c"
-	read lang
-	
-	if [ "$lang" == "C" ]; then
-		cd "/mnt/d/Programming/Data Structures/Data-Structures-and-Algorithms/Data_Structures/CLang"
-	elif [ "$lang" == "Cpp" ]; then
-		cd "/mnt/d/Programming/Data Structures/Data-Structures-and-Algorithms/Data_Structures/Cpp"
-	else
-		echo "Lang not found"
-	fi
+    dirfound=0
 
-elif [ "$dir" == "Algorithms" ]; then
-
-	echo -e "Which lang? \c"
+    case "$dir" in 
+    "DataStructures" | "data*" | "datastructures" | "Da*" | "da*" | "Data*" )
+        dirfound=1
+        echo -e "Which lang? \c"
         read lang
 
-	if [ "$lang" == "C" ]; then
-                cd "/mnt/d/Programming/Data Structures/Data-Structures-and-Algorithms/Algorithms/CLang"
-        elif [ "$lang" == "Cpp" ]; then
-                cd "/mnt/d/Programming/Data Structures/Data-Structures-and-Algorithms/Algorithms/Cpp"
-        else
+        while true; do
+            if [ "$lang" == "C" ]; then
+                cd "/mnt/d/Programming/Data Structures/Data-Structures-and-Algorithms/Data_Structures/CLang"
+                break
+            elif [ "$lang" == "Cpp" ]; then
+                cd "/mnt/d/Programming/Data Structures/Data-Structures-and-Algorithms/Data_Structures/Cpp"
+                break
+            else
                 echo "Lang not found"
-        fi
+                continue
+            fi
+        done
+        ;;
+    esac
 
-elif [ "$dir" == "Diploma" ]; then
-	cd "/mnt/d/Programming/Diploma"
+    if [ "$dirfound" == 1 ]; then
+        break
+    fi
 
-elif [ "$dir" == "ProblemSolving" ]; then
+    case "$dir" in
+    "Algorithms" | "algorithms" | "Alg*" | "alg*" )
+        dirfound=1
+        echo -e "Which lang? \c"
+        read lang
 
-	echo -e "Which Website? \c"
-	read website
+        while true; do
+            if [ "$lang" == "C" ]; then
+                cd "/mnt/d/Programming/Data Structures/Data-Structures-and-Algorithms/Algorithms/CLang"
+                break
+            elif [ "$lang" == "Cpp" ]; then
+                cd "/mnt/d/Programming/Data Structures/Data-Structures-and-Algorithms/Algorithms/Cpp"
+                break
+            else
+                echo "Lang not found"
+                continue
+            fi
+        done
+        ;;
+    esac
 
-	if [ "$website" == "LeetCode" ]; then
+    if [ "$dirfound" == 1 ]; then
+        break
+    fi
 
-		echo -e "Which lang? \c"
-		read lang
+    case  "$dir" in
+    "Diploma" | "diploma" | "Di*" | "di*" )
+        dirfound=1
+        cd "/mnt/d/Programming/Diploma"
+        ;;
+    esac
 
-		if [ "$lang" == "C" ]; then
-			cd "/mnt/d/Programming/ProblemSolving/LeetCode/C_lang"
-		elif [ "$lang" == "Cpp" ]; then
-			cd "/mnt/d/Programming/ProblemSolving/LeetCode/Cpp"
-		elif [ "$lang" == "Python" ]; then
-			cd "/mnt/d/Programming/ProblemSolving/LeetCode/Python"
-		else
-			echo "Lang not found"
-		fi
+    if [ "$dirfound" == 1 ]; then
+        break
+    fi
+
+    case  "$dir" in 
+    "ProblemSolving" | "problemsolving" | "pr*" | "Pr*" | "problem*" | "Problem*" )
+        dirfound=1
+		while true; do
+        echo -e "Which Website? \c"
+        read website
+        webfound=0
+				case "$website" in 
+				"LeetCode" | "leetcode" | "Le*" | "le*" | "leet*" | "Leet*")
+					webfound=1
+					while true; do
+						echo -e "Which lang? \c"
+						read lang
+						if [ "$lang" == "C" ]; then
+							cd "/mnt/d/Programming/ProblemSolving/LeetCode/C_lang"
+							break
+						elif [ "$lang" == "Cpp" ]; then
+							cd "/mnt/d/Programming/ProblemSolving/LeetCode/Cpp"
+							break
+						elif [ "$lang" == "Python" ]; then
+							cd "/mnt/d/Programming/ProblemSolving/LeetCode/Python"
+							break
+						else
+							echo "Lang not found"
+							continue
+						fi
+					done
+					;;
+				esac
+				if [ "$webfound" == 0 ]; then
+					echo "Website not found"
+					continue
+				else
+					break
+        		fi
+		done
+        ;;
+    esac
+	if [ "$dirfound" == 0 ]; then
+		echo "Dir not found"
 	else
-		echo "Website not found"
+		break
 	fi
-else 
-	echo "Dir not found"
-fi
+done 
+
